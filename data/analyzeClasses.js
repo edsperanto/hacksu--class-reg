c63d9bf04963b994321d13fefb8db2a58491e256
Edward Gao <edsperanto@users.noreply.github.com>
2017-11-11 16:43:56 -0800

    analyze unique classes and subject count

---
+++ data/analyzeClasses.js
@@ -0,0 +1,23 @@
+const data = require('./18WQ.json')['data'];
+
+var uniqueNames = data.reduce((arr, curr) => {
+	if(arr.indexOf(curr['name']) == -1) {
+		arr.push(curr['name']);
+		return arr;
+	}else{
+		return arr;
+	}
+}, []);
+
+var uniqueSubj = uniqueNames.reduce((arr, curr) => {
+	let currSubj = curr.split('-')[0];
+	if(arr.indexOf(currSubj) == -1) {
+		arr.push(currSubj);
+		return arr;
+	}else{
+		return arr;
+	}
+}, []);
+
+console.log(`There are ${uniqueNames.length} unique classes`);
+console.log(`There are ${uniqueSubj.length} unique subjects`);
