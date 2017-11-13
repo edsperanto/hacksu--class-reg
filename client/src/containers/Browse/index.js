74052e006ce57a9543d576d2d40e27e5671f3050
Edward Gao <edsperanto@users.noreply.github.com>
2017-11-13 02:27:44 +0000

    week view somewhat working

--- client/src/containers/Browse/index.js
+++ client/src/containers/Browse/index.js
@@ -39,20 +39,55 @@ class Browse extends Component {
 					<div id="browse-filters">· · · Filters</div>
 				</div>
 				<div id="browse-right">
-					<div className="week-columns" id="U-col">
-					</div>
-					<div className="week-columns" id="M-col">
-					</div>
-					<div className="week-columns" id="T-col">
-					</div>
-					<div className="week-columns" id="W-col">
-					</div>
-					<div className="week-columns" id="R-col">
-					</div>
-					<div className="week-columns" id="F-col">
-					</div>
-					<div className="week-columns" id="S-col">
-					</div>
+					{
+						['U', 'M', 'T', 'W', 'R', 'F', 'S'].map(day => {
+							return(
+								<div className="week-columns" id={day}>
+								{
+									this.props.candidates
+										.map(candidate => {
+											let parts = candidate.split('-');
+											let cName = `${parts[0]}-${parts[1]}`;
+											let cSection = parts[2];
+											return this.props.allClass
+												.filter(course => {
+													let matchName = course.name == cName;
+													let matchSec = course.section == cSection;
+													return matchName && matchSec;
+												})[0];
+										})
+										.filter(course => course.days.indexOf(day) > -1)
+										.map(course => {
+											let startTime = course['start time'];
+											let endTime = course['end time'];
+											function to24(str) {
+												let apm = str.indexOf('AM') > -1;
+												let hour = parseInt(str.split(':')[0]) + (apm?0:12);
+												let min = parseInt(str.split(':')[1].slice(0,2));
+												return hour + (min/60);
+											}
+											startTime = to24(startTime);
+											endTime = to24(endTime);
+											console.log('startTime', startTime);
+											console.log('endTime', endTime);
+											let myTop = `${startTime/60*100}%`;
+											let myHeight = `${(endTime-startTime)/24*100}%`;
+											let divStyle = {
+												top: myTop,
+												height: myHeight,
+												position: 'relative',
+												backgroundColor: 'gray'
+											}
+											return(
+												<div className="display-card" style={divStyle} key={Math.random()}>
+												</div>
+											)
+										})
+								}
+								</div>
+							)
+						})
+					}
 				</div>
 			</div>
 		)
